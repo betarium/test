caae397f9c045f791de07d88a62991568b6acb7c
betarium <betarium@users.noreply.github.com>
2015-07-29 14:41:55 +0900

    md5w追加

---
+++ vcs/md5w/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace md5w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            string path = args[0];
+            var bytes = File.ReadAllBytes(path);
+            var md5service = MD5CryptoServiceProvider.Create();
+            var hash = md5service.ComputeHash(bytes);
+
+            StringBuilder hashStr = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hashStr.AppendFormat("{0:X2}", hash[i]);
+            }
+
+            System.Diagnostics.Debug.WriteLine(hashStr);
+            System.Console.WriteLine(hashStr);
+        }
+    }
+}
