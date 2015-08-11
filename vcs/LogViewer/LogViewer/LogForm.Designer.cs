5a8e253b4293eae6bd8ab8f23271420b14bf76a8
betarium <betarium@users.noreply.github.com>
2015-08-11 15:04:53 +0900

    LogViewer追加

---
+++ vcs/LogViewer/LogViewer/LogForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LogViewer
+{
+    partial class LogForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.logTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logTextBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.logTextBox.Location = new System.Drawing.Point(0, 0);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(806, 424);
+            this.logTextBox.TabIndex = 0;
+            this.logTextBox.WordWrap = false;
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Interval = 10000;
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // LogForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 424);
+            this.Controls.Add(this.logTextBox);
+            this.Name = "LogForm";
+            this.Text = "ログビューア";
+            this.Load += new System.EventHandler(this.LogForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.LogForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.LogForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Timer refreshTimer;
+    }
+}
+
---
+++ vcs/LogViewer/LogViewer/LogForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LogViewer
+{
+    public partial class LogForm : Form
+    {
+        public string LogFile { get; set; }
+        public DateTime LogFileModified { get; set; }
+        public string DefaultTitle { get; set; }
+
+        public LogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LogForm_Load(object sender, EventArgs e)
+        {
+            DefaultTitle = Text;
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length >= 2)
+            {
+                LoadFile(args[1]);
+                Visible = true;
+                logTextBox.ScrollToCaret();
+            }
+        }
+
+        protected void LoadFile(string file)
+        {
+            try
+            {
+                //                string text = File.ReadAllText(file, Encoding.Default);
+
+                using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (TextReader reader = new StreamReader(stream, Encoding.Default))
+                    {
+                        string text = reader.ReadToEnd();
+                        logTextBox.Text = text;
+                    }
+                }
+
+                logTextBox.SelectionStart = logTextBox.Text.Length;
+                logTextBox.ScrollToCaret();
+                LogFileModified = File.GetLastWriteTime(file);
+                LogFile = file;
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show(ioe.Message);
+                if (LogFile == file)
+                {
+                    LogFile = null;
+                }
+                return;
+            }
+            Text = Path.GetFileName(file) + " - " + LogFileModified.ToString("yyyy/MM/dd HH:mm:ss") + " - " + DefaultTitle;
+            refreshTimer.Start();
+        }
+
+        private void LogForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void LogForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] drags = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                string path = drags[0];
+                e.Effect = DragDropEffects.Copy;
+                LoadFile(path);
+            }
+        }
+
+        private void refreshTimer_Tick(object sender, EventArgs e)
+        {
+            if (LogFile != null)
+            {
+                if (!File.Exists(LogFile))
+                {
+                    return;
+                }
+                DateTime modified = File.GetLastWriteTime(LogFile);
+                if (modified != LogFileModified)
+                {
+                    LoadFile(LogFile);
+                }
+            }
+        }
+
+    }
+}
