c9be4b1e333f3da76e5a8c73534fd4486685642c
betarium <betarium@users.noreply.github.com>
2016-08-16 18:19:25 +0900

    add base64

---
+++ vcs/base64/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace base64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            using (TextReader reader = new StreamReader(path, Encoding.Default))
+            {
+                string data = reader.ReadToEnd();
+                byte[] val = Convert.FromBase64String(data);
+                using (FileStream stream = new FileStream(path + ".bin", FileMode.Create))
+                {
+                    stream.Write(val, 0, val.Length);
+                }
+            }
+        }
+    }
+}
